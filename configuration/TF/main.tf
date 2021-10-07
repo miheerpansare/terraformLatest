81bee65ba24f779de65c67b8b83207f0bf0863fb
Miheer Pansare1 <miheer.pansare1@globallogic.com>
2021-10-07 15:37:08 +0530

    ninth COMMIT

--- configuration/TF/main.tf
+++ configuration/TF/main.tf
@@ -34,7 +34,6 @@ resource "vsphere_virtual_machine" "vm" {
   name             = var.vm_name
   resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
-  source = "https://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64.ova"
   num_cpus = 1
   memory   = 512
   guest_id = "other3xLinux64Guest"
