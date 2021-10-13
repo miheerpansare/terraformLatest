7a62ee410c87927968545423727a1877dc509fed
Miheer Pansare1 <miheer.pansare1@globallogic.com>
2021-10-13 09:25:36 +0530

    25th commit

--- configuration/TF/main.tf
+++ configuration/TF/main.tf
@@ -41,6 +41,17 @@ data "vsphere_resource_pool" "pool" {
 }
 
 
+data "vsphere_ovf_vm_template" "ovf" {
+  name             = "testOVF"
+  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+   host_system_id = "${data.vsphere_host.host.id}"	
+  remote_ovf_url   = "https://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64.ova"
+
+  ovf_network_map = {
+    "Network 1": "${data.vsphere_network.network.id}"
+  }
+}
 
 resource "vsphere_virtual_machine" "vm" {
   name             = var.vm_name
@@ -48,17 +59,23 @@ resource "vsphere_virtual_machine" "vm" {
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
   num_cpus = 1
   memory   = 512
-  guest_id = "other3xLinux64Guest"
   wait_for_guest_net_timeout = 0
   wait_for_guest_ip_timeout  = 0	
   host_system_id = "${data.vsphere_host.host.id}"	
   network_interface {
     network_id = "${data.vsphere_network.network.id}"
   }
+ 
+  dynamic "network_interface" {
+    for_each = "${data.vsphere_ovf_vm_template.ovf.ovf_network_map}"
+    content {
+      network_id = network_interface.value
+    }
+  }
+
   ovf_deploy {
-    allow_unverified_ssl_cert = true
-    // Url to remote ovf/ova file
-    remote_ovf_url = "https://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64.ova"
+    ovf_network_map = "${data.vsphere_ovf_vm_template.ovf.ovf_network_map}"
+    remote_ovf_url  = "${data.vsphere_ovf_vm_template.ovf.remote_ovf_url}"
   }
 
   disk {
