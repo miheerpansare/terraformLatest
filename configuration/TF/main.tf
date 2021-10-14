c555cf81d6a44cf8910020c71bf77314dbed0690
Miheer Pansare1 <miheer.pansare1@globallogic.com>
2021-10-14 15:57:48 +0530

    29th commit

--- configuration/TF/main.tf
+++ configuration/TF/main.tf
@@ -44,6 +44,7 @@ resource "vsphere_virtual_machine" "vm" {
   name             = var.vm_name
   resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  datacenter_id = "${data.vsphere_datacenter.datacenter.id}"
   num_cpus = 1
   memory   = 512
   wait_for_guest_net_timeout = 0
